7926624fc7a598a9cd030e014b0598342f09707f
Gagun45 <selyanchyn45@gmail.com>
2025-12-10 17:04:23 +0200

    lesson2 task1 edited

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,8 +1,8 @@
 import type {ITodo} from "../models/TodoModel.ts";
 
-const baseUrl = 'https://jsonplaceholder.typicode.com/todos'
+const baseUrl = 'https://jsonplaceholder.typicode.com'
 
 export const getTodos = async ():Promise<ITodo[]> => {
-    const res = await fetch(baseUrl)
+    const res = await fetch(`${baseUrl}/todos`)
     return await res.json()
 }
