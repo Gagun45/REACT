6b78638a9ea2fd9174db97c69d1c3174afcfb1bc
Gagun45 <selyanchyn45@gmail.com>
2025-12-12 12:06:13 +0200

    lesson3 task1 part2 (via browser router) done

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import {createRoot} from "react-dom/client";
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./routes/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./layouts/Layout.tsx";
+import {UsersComponent} from "./components/users/UsersComponent.tsx";
+import {PostsComponent} from "./components/posts/PostsComponent.tsx";
+import {CommentsComponent} from "./components/comments/CommentsComponent.tsx";
+import {ProductsComponent} from "./components/products/ProductsComponent.tsx";
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>
+createRoot(document.getElementById('root')!).render(<BrowserRouter>
+    <Routes>
+        <Route path={'/'} element={<Layout/>}>
+            <Route path={'users'} element={<UsersComponent/>}/>
+            <Route path={'posts'} element={<PostsComponent/>}/>
+            <Route path={'comments'} element={<CommentsComponent/>}/>
+            <Route path={'products'} element={<ProductsComponent/>}/>
+        </Route>
+    </Routes>
+    </BrowserRouter>
 )
